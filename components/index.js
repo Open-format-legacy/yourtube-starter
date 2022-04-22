cbf8ad9dcb1f9616c677d33db674f69345fc4ce6
Andy <andy.kitt@simpleweb.co.uk>
2022-04-22 12:09:09 +0100

    fetch creator and add avatar component to video grid

--- components/index.js
+++ components/index.js
@@ -1,7 +1,8 @@
+import Avatar from "./avatar";
 import Button from "./button";
 import Header from "./header";
 import Input from "./input";
 import VideoGrid from "./videoGrid";
 import VideoPlayer from "./videoPlayer";
 
-export { Button, Header, Input, VideoGrid, VideoPlayer };
+export { Avatar, Button, Header, Input, VideoGrid, VideoPlayer };
